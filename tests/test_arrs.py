7a01c65361d393c1038a624ebf59d06de074f1eb
maksfrol96 <maxfrol96@mail.ru>
2023-02-04 21:58:21 +0300

    100% покрытие

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -4,11 +4,16 @@ from utils import arrs
 def test_get():
     assert arrs.get([1, 2, 3], 1, "test") == 2
     assert arrs.get([], 0, "test") == "test"
+    assert arrs.get([], -2, "test") == "test"
 
 
 def test_slice():
     assert arrs.my_slice([1, 2, 3, 4], 1, 3) == [2, 3]
     assert arrs.my_slice([1, 2, 3], 1) == [2, 3]
+    assert arrs.my_slice([1, 2, 3], -1) == [3]
+    assert arrs.my_slice([]) == []
+    assert arrs.my_slice([1,2,3],-6, 2) ==  [1,2]
+
 
 test_get()
 test_slice()
